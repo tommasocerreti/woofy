05245bed29b71a1f5ee8610b61e268a275c4e01f
tommasocerreti <tommasocerreti@libero.it>
2023-05-17 21:52:04 +0200

    Aggiunti commenti nei file script.js

--- public/home/script.js
+++ public/home/script.js
@@ -1,3 +1,4 @@
+// NAVBAR - SCROLL
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
     if (window.pageYOffset > 0) {
--- public/login/script.js
+++ public/login/script.js
@@ -1,8 +1,9 @@
+// NAVBAR - SCROLL
 window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.pageYOffset > 0) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
+  var navbar = document.getElementById('navbar');
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
 });
