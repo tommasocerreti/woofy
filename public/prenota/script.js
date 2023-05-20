e98a4768abecdaea0a97a724f234605c8ca8eaec
Alessio Canuti <126820024+BadulaNut@users.noreply.github.com>
2023-05-20 16:59:57 +0200

    inizio ridimensionamento

--- public/prenota/script.js
+++ public/prenota/script.js
@@ -7,3 +7,13 @@ window.addEventListener('scroll', function() {
     navbar.classList.remove('scrolled');
   }
 });
+
+// RIDIMENSIONAMENTO
+function setBodyScale() {
+  const viewportWidth = window.innerWidth;
+  const scale = Math.min(viewportWidth / 1000, 1);
+  document.body.style.transform = `scale(${scale})`;
+}
+
+window.addEventListener('resize', setBodyScale);
+setBodyScale();
