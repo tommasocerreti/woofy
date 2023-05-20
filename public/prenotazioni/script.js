49e2e85d3c66b719093bf2a0a2fc430af96bd6c1
Alessio Canuti <126820024+BadulaNut@users.noreply.github.com>
2023-05-20 21:10:05 +0200

    modifiche @media in prenota

--- public/prenotazioni/script.js
+++ public/prenotazioni/script.js
@@ -7,13 +7,3 @@ window.addEventListener('scroll', function() {
     navbar.classList.remove('scrolled');
   }
 });
-
-// RIDIMENSIONAMENTO
-function setBodyScale() {
-  const viewportWidth = window.innerWidth;
-  const scale = Math.min(viewportWidth / 1000, 1);
-  document.body.style.transform = `scale(${scale})`;
-}
-
-window.addEventListener('resize', setBodyScale);
-setBodyScale();
