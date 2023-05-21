6120395efd629b5a63a676ce43eb2b74c83519f5
Alessio Canuti <126820024+BadulaNut@users.noreply.github.com>
2023-05-21 18:32:52 +0200

    fixato problema firefox prenota

--- public/login/script.js
+++ public/login/script.js
@@ -1,13 +1,3 @@
-// NAVBAR - SCROLL
-window.addEventListener('scroll', function() {
-  var navbar = document.getElementById('navbar');
-  if (window.pageYOffset > 0) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
-
 // RIDIMENSIONAMENTO
 function setBodyScale() {
   const viewportWidth = window.innerWidth;
--- public/prenotazioni/script.js
+++
@@ -1,9 +0,0 @@
-// NAVBAR - SCROLL
-window.addEventListener('scroll', function() {
-  var navbar = document.getElementById('navbar');
-  if (window.pageYOffset > 0) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
